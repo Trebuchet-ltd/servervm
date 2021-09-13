85e862c23047fb264298a73884d48eaaf4e776a5
sunith vs <sunithvazhenkada@gmail.com>
2021-09-14 05:00:11 +0530

    cleaned code

--- home/extra_functions.py
+++ home/extra_functions.py
@@ -1,10 +1,10 @@
 import libvirt
 import re
-import pexpect
 import os
 import subprocess
 import time
 from .models import Storage
+import math
 
 
 def set_storage(instance):
@@ -18,17 +18,23 @@ def set_storage(instance):
     :return:
     """
 
-    storage = instance.storage
+    storage = int(math.fabs(instance.storage))
     os.system(
-        f"sudo qemu-img create -f qcow2 -o preallocation=metadata /var/kvm/images/{instance.name}-new.qcow2 {storage}G")
-    os.system(f"sudo chown libvirt-qemu:kvm /var/kvm/images/{instance.name}-new.qcow2")
-    os.system(
-        f"sudo virt-resize --expand /dev/vda3 /var/kvm/images/{instance.name}.qcow2 /var/kvm/images/{instance.name}-new.qcow2")
-    os.system(f"sudo rm /var/kvm/images/{instance.name}.qcow2")
-    os.system(f"sudo mv /var/kvm/images/{instance.name}-new.qcow2 /var/kvm/images/{instance.name}.qcow2")
+        f"sudo qemu-img create -f qcow2 -o preallocation=metadata /var/kvm/images/{instance.code}-new.qcow2 {storage}G")
+    os.system(f"sudo chown libvirt-qemu:kvm /var/kvm/images/{instance.code}-new.qcow2")
+
+    if instance.os == "ubuntu_20.04":
+        os.system(
+            f"sudo virt-resize --expand /dev/vda3 /var/kvm/images/{instance.code}.qcow2 /var/kvm/images/{instance.code}-new.qcow2")
+    elif instance.os == "centos_8":
+        os.system(
+            f"sudo virt-resize --expand /dev/vda2 /var/kvm/images/{instance.code}.qcow2 /var/kvm/images/{instance.code}-new.qcow2")
+
+    os.system(f"sudo rm /var/kvm/images/{instance.code}.qcow2")
+    os.system(f"sudo mv /var/kvm/images/{instance.code}-new.qcow2 /var/kvm/images/{instance.code}.qcow2")
 
 
-def set_memory(instance, dom,vcpu=True):
+def set_memory(instance, dom, vcpu=True):
     """
     --->  This function first change the max memory
     --->  Then change the permission to qmeu:kvm
@@ -41,42 +47,54 @@ def set_memory(instance, dom,vcpu=True):
     :return:
     """
 
-    ram = instance.memory
+    ram = int(math.fabs(instance.memory))
+    vcpus = int(math.fabs(instance.vcpus))
+
     dom.setMaxMemory(int(ram) * 1024 * 1024)
-    os.system(f"virsh setvcpus {instance.name} {instance.vcpus} --config --maximum")
-    os.system(f"virsh setvcpus {instance.name} {instance.vcpus} --config ")
-    os.system(f"virsh start {instance.name}")
-    os.system(f"virsh setvcpus --count {instance.vcpus} {instance.name} ")
+    if vcpus:
+        os.system(f"virsh setvcpus {instance.code} {vcpus} --config --maximum")
+        os.system(f"virsh setvcpus {instance.code} {vcpus} --config ")
+    os.system(f"virsh start {instance.code}")
+    if vcpus:
+        os.system(f"virsh setvcpus --count {vcpus} {instance.code} ")
     time.sleep(5)
     dom.setMemory(int(ram) * 1024 * 1024)
 
 
 def create_vm(instance):
-    command = f"virt-clone --original ubuntu2004 --name {instance.name} --file /var/kvm/images/{instance.name}.qcow2"
+
+    if instance.os == "ubuntu_20.04":
+        command = f"virt-clone --original ubuntu2004 --name {instance.code} --file /var/kvm/images/{instance.code}.qcow2"
+    elif instance.os == "centos_8":
+        command = f"virt-clone --original centos8 --name {instance.code} --file /var/kvm/images/{instance.code}.qcow2"
+    else:
+        command = "echo 'invalid option'"
+
     os.system(command)
     conn = libvirt.open("qemu:///system")
-    dom = conn.lookupByName(instance.name)
+    dom = conn.lookupByName(instance.code)
 
-    set_storage(instance)
+    if instance.storage != 20:
+        set_storage(instance)
+
     set_memory(instance, dom)
     conn.close()
     mac_address = re.search(r"<mac address='([A-Za-z0-9:]+)'", dom.XMLDesc(0)).groups()[0]
     instance.mac_address = mac_address
-    Storage.objects.create(vm=instance, size=20, file_path=f"/var/kvm/images/{instance.name}.img")
+    Storage.objects.create(vm=instance, size=20, file_path=f"/var/kvm/images/{instance.code}.img")
     instance.save()
 
 
 def delete_vm(instance):
-    os.system(f"rm /home/ubuntu/{instance.name}")
-    os.system(f"rm /home/ubuntu/{instance.name}.pub")
-
+    # os.system(f"rm /home/ubuntu/servervm/media/{instance.user}/{instance.code}")
+    # os.system(f"rm /home/ubuntu/servervm/media/{instance.user}/{instance.code}.pub")
     conn = libvirt.open("qemu:///system")
     try:
-        dom = conn.lookupByName(instance.name)
+        dom = conn.lookupByName(instance.code)
         if dom.isActive():
-            os.system(f"virsh shutdown {instance.name}")
+            os.system(f"virsh shutdown {instance.code}")
             time.sleep(10)
-        os.system(f"virsh undefine {instance.name} --remove-all-storage")
+        os.system(f"virsh undefine {instance.code} --remove-all-storage")
     except Exception as e:
         print(e)
     instance.delete()
@@ -84,9 +102,9 @@ def delete_vm(instance):
 
 def update_vm(instance):
     conn = libvirt.open("qemu:///system")
-    dom = conn.lookupByName(instance.name)
+    dom = conn.lookupByName(instance.code)
 
-    os.system(f"virsh shutdown {instance.name}")
+    os.system(f"virsh shutdown {instance.code}")
     time.sleep(20)
     print("shut down completed")
     set_storage(instance)
--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+import string
+import random
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class PemFile(models.Model):
@@ -9,16 +12,38 @@ class PemFile(models.Model):
     file_path = models.TextField()
     created_date = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user} {self.name}"
+
+
+def code_generator(size=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def create_new_code():
+    not_unique = True
+    unique_code = code_generator()
+    while not_unique:
+        unique_code = code_generator()
+        if not VirtualMachine.objects.filter(code=unique_code):
+            not_unique = False
+    return str(unique_code)
+
 
 class VirtualMachine(models.Model):
-    user = models.ForeignKey(User,related_name="vm", on_delete=models.CASCADE)
+    available_os = (('ubuntu_20.04',"ubuntu_20.04",), ("centos_8", "centos_8"))
+    user = models.ForeignKey(User, related_name="vm", on_delete=models.CASCADE)
+    code = models.CharField(max_length=50, default=create_new_code, blank=True, null=True)
     name = models.CharField(max_length=25)
-    memory = models.IntegerField(default=4, help_text="in GB")
-    vcpus = models.IntegerField(default=2)
-    storage = models.IntegerField(default=20, help_text="in GB")
+    memory = models.PositiveIntegerField(default=4, help_text="in GB",
+                                         validators=[MinValueValidator(1), MaxValueValidator(16)])
+    vcpus = models.PositiveIntegerField(default=2, validators=[MinValueValidator(1), MaxValueValidator(20)])
+    storage = models.PositiveIntegerField(default=20, help_text="in GB",
+                                          validators=[MinValueValidator(5), MaxValueValidator(100)])
+
     mac_address = models.CharField(max_length=50, blank=True, null=True)
     ip_address = models.GenericIPAddressField(blank=True, null=True)
-    os = models.CharField(max_length=30)
+    os=models.CharField(max_length=20,choices=available_os)
     vpn_ip = models.GenericIPAddressField(blank=True, null=True)
     virtual_mac = models.CharField(max_length=50, blank=True, null=True)
     pem_file = models.ForeignKey(PemFile, on_delete=models.PROTECT, related_name="vm", null=True, blank=True)
