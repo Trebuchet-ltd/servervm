fb661d665e4868ed0f169bb6d9d995091288242d
sunith vs <sunithvazhenkada@gmail.com>
2021-09-14 05:05:37 +0530

    cleaned code

--- home/views.py
+++ home/views.py
@@ -2,20 +2,14 @@
 from django.http import FileResponse
 from rest_framework.decorators import action
 
-from .serializers import VirtualMachineSerializer,PemFileSerializer
-from .models import VirtualMachine,PemFile
-from django.contrib.auth.models import User
-from rest_framework import viewsets, status, filters
-# from rest_framework import permissions
-# from rest_framework.decorators import action
-from rest_framework.decorators import api_view
+from .serializers import VirtualMachineSerializer, PemFileSerializer
+from .models import VirtualMachine, PemFile
+from rest_framework import viewsets
 from rest_framework.response import Response
-# import sys
 from .extra_functions import *
-# Create your views here.
 import threading
 import time
-
+import servervm.settings as settings
 
 
 class VmViewSet(viewsets.ModelViewSet):
@@ -33,28 +27,30 @@ class VmViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         print(serializer.validated_data)
         instance = serializer.save(user=self.request.user)
-        # print(f"{instance}")
-        # threading.Thread(target=update_vm, args=(instance,)).start()
+        threading.Thread(target=update_vm, args=(instance,)).start()
 
     @action(methods=['post'], detail=False)
     def update_ip(self, request):
+        print(request.data)
         mac_address = request.data["mac_address"]
         vm = VirtualMachine.objects.get(mac_address=mac_address)
-        vm.ip_address = request.data["ip_address"]
-        vm.save()
-        os.system(f"ssh-keygen -q -t rsa -N '' -f /home/ubuntu/{vm.name} <<y")
-        time.sleep(10)
-        os.system(f"sshpass -p '12345678' ssh-copy-id -f -i {vm.pem_file.file_path}.pub -o StrictHostKeyChecking=no test@{vm.ip_address} ")
-        os.system(f"sshpass -p '12345678' ssh test@{vm.ip_address} bash /var/local/remove_password.sh > err.html ")
+        if not vm.ip_address:
+            vm.ip_address = request.data["ip_address"]
+            vm.save()
+            os.system(f"ssh-keygen -q -t rsa -N '' -f /home/ubuntu/{vm.name} <<y")
+            time.sleep(10)
+            os.system(f"sshpass -p '{settings.vm_template_password}' ssh-copy-id -f -i {vm.pem_file.file_path}.pub -o StrictHostKeyChecking=no user@{vm.ip_address} ")
+            os.system(f"sshpass -p '{settings.vm_template_password}' ssh -o StrictHostKeyChecking=no user@{vm.ip_address} bash /var/local/setup.sh > err.html ")
         return Response(status=201)
 
     @action(methods=['post'], detail=False)
     def set_vpn_ip(self, request):
         mac_address = request.POST["mac_address"]
         vm = VirtualMachine.objects.get(mac_address=mac_address)
-        vm.vpn_ip = request.POST["vpn_ip"]
-        vm.virtual_mac = request.POST["virtual_mac"]
-        vm.save()
+        if not vm.vpn_ip:
+            vm.vpn_ip = request.POST["vpn_ip"]
+            vm.virtual_mac = request.POST["virtual_mac"]
+            vm.save()
         return Response(status=201)
 
 
@@ -71,16 +67,15 @@ class PemFileViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         name = self.request.data['name']
         os.makedirs(f"/home/ubuntu/servervm/media/{self.request.user}", exist_ok=True)
-
-        os.system(f"ssh-keygen -q -t rsa -N '' -f /home/ubuntu/servervm/media/{self.request.user}/{name} <<y")
-        serializer.save(user=self.request.user,
-                        file_path=f"/home/ubuntu/servervm/media/{self.request.user}/{name}")
+        instance = serializer.save(user=self.request.user,
+                                   file_path=f"/home/ubuntu/servervm/media/{self.request.user}/{name}")
+        os.system(f"ssh-keygen -q -t rsa -N '' -f /home/ubuntu/servervm/media/{self.request.user.id}/{instance.id} <<y")
 
     @action(methods=["get"], detail=True)
     def download_file(self, request, pk):
         pem_file = PemFile.objects.get(pk=pk)
         pem = open(pem_file.file_path, 'rb')
-        response = FileResponse(pem)
+        response = FileResponse(pem, filename=pem_file.name)
         os.remove(pem_file.file_path)
         return response
 
